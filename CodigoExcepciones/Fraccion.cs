523226945f09743a33cc657f86616b273138d8f8
dotCero <lucero.inginfo@gmail.com>
2021-10-07 13:45:29 -0300

    Agregar archivos de proyecto.

---
+++ CodigoExcepciones/Fraccion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoExcepciones
+{
+    class Fraccion
+    {
+        private int numerador;
+        private int denominador;
+
+        public Fraccion(int numerador, int denominador)
+        {
+            this.numerador = numerador;
+            this.denominador = denominador;
+        }
+
+        public int Numerador { get => numerador; set => numerador = value; }
+        public int Denominador { get => denominador; set => denominador = value; }
+
+        public string MostrarFraccion()
+        {
+            string fracc = numerador+"/"+denominador;
+            return fracc;
+        }
+
+        public double valorFraccion()
+        {
+            //DivideByZeroException
+            double resultado;
+            try
+            {
+                resultado = numerador / denominador;
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("El resultado es imposible de mostrar, porque el denominador es actualmente un 0, y la división por 0, no está definida");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                resultado = 0;
+            }
+            finally
+            {
+                Console.WriteLine("El Código sigue su ejecución...");
+            }
+            
+            return resultado;
+        }
+    }
+}
---
+++ CodigoExcepciones/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodigoExcepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> miListaNumeros = new List<int>();
+
+            miListaNumeros.Add(2);
+            miListaNumeros.Add(5);
+            miListaNumeros.Add(2001);
+
+            Console.WriteLine("La lista tiene {0} elementos", miListaNumeros.Count);
+
+            Console.WriteLine(miListaNumeros[0]);
+            Console.WriteLine(miListaNumeros[1]);
+            Console.WriteLine(miListaNumeros[2]);
+
+            //Intento ejecutar un código que posiblemente podría caer una excepción
+            //ArgumentOutOfRangeException
+            try
+            {
+                Console.WriteLine(miListaNumeros[3]);
+            }
+            //si no puede ejecutar, directamente se ejecuta el catch que captura la excepción lanzada
+            //si la excepción lanzada no se encuentra controlada, el código dejará de ejecutarse igualmente.
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("No se puede acceder a la posición");
+                Console.WriteLine("La lista solo tiene {0} elementos", miListaNumeros.Count);
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("El programa dejó de ejecutarse por alguna razon");
+            }
+
+            Console.WriteLine("El código a continuado luego del bloque try-catch");
+
+            Console.WriteLine("****************");
+
+            Fraccion nuevaFraccion = new Fraccion(4, 0);
+            Console.WriteLine(nuevaFraccion.MostrarFraccion());
+            Console.WriteLine("El resultado de la fracción es: {0}", nuevaFraccion.valorFraccion());
+        }
+    }
+}
